ce5fd4d885f99a4ea9b0234efa80668080efa0f4
dima <dkubarev88@gmail.com>
2022-01-21 15:11:58 +0300

    add social account db model

--- src/models/db/auth_model.py
+++ src/models/db/auth_model.py
@@ -4,6 +4,7 @@ import datetime
 import uuid
 
 from sqlalchemy import DateTime
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import UUIDType
 
@@ -87,3 +88,20 @@ class User(Base):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+class SocialAccount(db.Model):
+    __tablename__ = 'social_account'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship(User, backref=db.backref('social_accounts', lazy=True))
+
+    social_id = db.Column(db.Text, nullable=False)
+    social_name = db.Column(db.Text, nullable=False)
+
+    __table_args__ = (
+    db.UniqueConstraint('social_id', 'social_name', name='social_pk'),)
+
+    def __repr__(self):
+        return f'<SocialAccount {self.social_name}:{self.user_id}>'
