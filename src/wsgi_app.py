8f8768b191fc3acf7097f1677c81f4e0cb68a159
Victor Merkushov <merkushov.victor@gmail.com>
2022-11-09 12:54:35 +0600

    Перенастройки автотестов. Исправление ошибок в настройках (#16)

--- src/wsgi_app.py
+++ src/wsgi_app.py
@@ -1,7 +1,6 @@
-from gevent import monkey
-
+from gevent.pywsgi import WSGIServer
 from app import create_app
 
-monkey.patch_all()
-
 app = create_app()
+http_server = WSGIServer(("0.0.0.0", 8000), app)
+http_server.serve_forever()
